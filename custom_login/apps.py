75e7df529aab550084befcae123815380d5f47c3
Milad Hatami <miladhzz@gmail.com>
2020-07-24 10:50:57 +0430

    add signals

--- custom_login/apps.py
+++ custom_login/apps.py
@@ -1,5 +1,14 @@
 from django.apps import AppConfig
+from django.core.signals import request_finished
+from django.db.models.signals import post_save
 
 
 class CustomLoginConfig(AppConfig):
     name = 'custom_login'
+
+    def ready(self):
+        from . import signals
+        from . import models
+        request_finished.connect(signals.my_callback)
+        post_save.connect(signals.create_new_user, sender=models.MyUser)
+        post_save.connect(signals.update_user, sender=models.MyUser)
